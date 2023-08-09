4a571185b39ff6e29c794c7eacd3a6ec7390ff33
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-09 18:08:38 +0900

    Challenge 7

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const grades = parseInt(prompt("성적을 입력 해주세요."));
+const h1 = document.querySelector(".hello h1");
+let a = 0;
 
-if (isNaN(grades) || grades < 0) {
-	alert("정확한 값을 입력 해주세요.");
-} else if (grades >= 90) {
-    alert('A');
-} else if (grades >= 80) {
-    alert('B');
-} else if (grades >= 70) {
-    alert('C');
-} else {
-    alert('D');
+function handleTitleClick() {
+    a++;
+    h1.innerText = "click : " + a;
 }
+
+h1.addEventListener("click", handleTitleClick);
