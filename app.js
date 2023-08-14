0244b2ef84c3663910935d84cf94782a7db3884f
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-14 22:04:31 +0900

    Challenge 10

--- app.js
+++ app.js
@@ -1,12 +1,37 @@
-const button = document.querySelector("#ckeck");
+// 임의 아이디 : jinsoo
+// 임의 비밀번호 : wlstn1578@
 
-function handleTitleClick() {
-    button.classList.toggle("clicked");
-    if (button.className === "clicked") {
-        button.innerText = "취소";
+const loginForm = document.querySelector("#login-form");
+const loginInputId = document.querySelector("#login-form input#Id");
+const loginInputPassword = document.querySelector("#login-form input#Password");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY_ID = "usernameId";
+const USERNAME_KEY_PW = "usernamePw";
+
+const savedUsernameID = localStorage.getItem(USERNAME_KEY_ID);
+const savedUsernamePW = localStorage.getItem(USERNAME_KEY_PW);
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const usernameId = loginInputId.value;
+    const usernamePw = loginInputPassword.value;
+    if (savedUsernameID === usernameId && savedUsernamePW === usernamePw) {
+        alert(`환영합니다 ${usernameId}님!`);
+        paintGreetings(usernameId);
     } else {
-        button.innerText = "확인";
+        alert("아이디 또는 비밀번호가 틀렸습니다. 다시 입력하세요.");
+        loginForm.classList.remove(HIDDEN_CLASSNAME);
+        loginForm.addEventListener("submit", onLoginSubmit);
     }
 }
 
-button.addEventListener("click", handleTitleClick);
+function paintGreetings(usernameId){
+    greeting.innerText = `Hello ${usernameId}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+loginForm.classList.remove(HIDDEN_CLASSNAME);
+loginForm.addEventListener("submit", onLoginSubmit);
