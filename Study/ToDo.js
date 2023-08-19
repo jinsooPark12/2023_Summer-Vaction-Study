4d32c7b65b6159e72e10cf1c439039833c761874
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2023-08-20 02:14:25 +0900

    Challenge 13

--- Study/ToDo.js
+++ Study/ToDo.js
@@ -3,22 +3,48 @@ const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const STRIKE_THOUGH = "strike-through";
+const TODOS_KEY = "todos";
+const CHECK_BOX = "check";
+
+let toDos = [];
+
+let checkBox = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
+
+function saveCheck() {
+    localStorage.setItem(CHECK_BOX, JSON.stringify(checkBox));
+}
+
+function switchCheckBox(id) {
+    const Check = document.getElementById(`${parseInt(id) + 1}`);
+    Check.checked = true;
+}
 
 function deleteToDo(event) {
     const li = event.target.parentElement;
-    if (event.target.checked === true) {
+    const is_checked = event.target.checked;
+    if (is_checked === true) {
         li.classList.add(STRIKE_THOUGH);
+        checkBox.push(li.id);
+        saveCheck();
     } else {
         li.classList.remove(STRIKE_THOUGH);
+        checkBox = checkBox.filter((check) => check !== li.id);
+        saveCheck();
     }
 }
 
 function paintToDo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
     const checkbox = document.createElement("INPUT");
+    checkbox.id = newTodo.id + 1;
     checkbox.setAttribute("type", "checkbox");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     checkbox.addEventListener("click", deleteToDo);
     li.appendChild(checkbox);
     li.appendChild(span);
@@ -29,8 +55,29 @@ function hadnleToDoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text : newTodo,
+        id : Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 }
 
 toDoForm.addEventListener("submit", hadnleToDoSubmit);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
+
+const savedCheck = localStorage.getItem(CHECK_BOX);
+
+if (savedToDos !== null) {
+    const parsedToDos = JSON.parse(savedToDos);
+    toDos = parsedToDos;
+    parsedToDos.forEach(paintToDo);
+}
+
+if (savedCheck !== null) {
+    const parsedCheck = JSON.parse(savedCheck);
+    checkBox = parsedCheck;
+    parsedCheck.forEach(switchCheckBox);
+}
--- Study/app.js
+++ Study/app.js
@@ -43,6 +43,7 @@ function paintGreetings(usernameId){
     greeting.classList.remove(HIDDEN_CLASSNAME);
     clock.classList.remove(HIDDEN_CLASSNAME);
     toDoForm.classList.remove(HIDDEN_CLASSNAME);
+    toDoList.classList.remove(HIDDEN_CLASSNAME);
     document.body.appendChild(bgImage);
     loginPoto.classList.add(HIDDEN_CLASSNAME);
 }
