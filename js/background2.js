51b96f61b19442e1722363435ad994dc2d2d079a
jinsooPark12 <141107376+jinsooPark12@users.noreply.github.com>
2024-04-16 22:31:02 +0900

    Delete js directory

--- js/background2.js
+++
@@ -1,9 +0,0 @@
-const images = ["img/0.jpeg", "img/1.jpeg", "img/2.jpeg"];
-
-const chosenImage = images[Math.floor(Math.random() * images.length)];
-
-function bgSelect(){
-    console.log([chosenImage]);
-    document.body.style.backgroundImage = `url(${[chosenImage]})`;
-  }
-  bgSelect();
